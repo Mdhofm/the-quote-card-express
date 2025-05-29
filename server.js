0f32e62d252a6c6729c7bc3866632419c708613b
Max Hofmann <maxhofmann1996@gmail.com>
2025-05-29 19:02:02 -0400

    Setup express to serve web page

--- server.js
+++ server.js
@@ -2,3 +2,18 @@
 
 const express = require("express");
 const app = express();
+
+//specifies port that application is served on
+const port = 8080;
+
+//specifies the directory where the express app will find the static files
+app.use(express.static("./public"));
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.listen(port, () => {
+    console.log(`Server is running http://localhost:${port}`);
+    console.log("Press Ctrl+C to end this process.");
+});
